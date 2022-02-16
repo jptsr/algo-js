c147add3d3b5f50f043d7807d62d8ddadb66f018
Julie <juptsr.becode@gmail.com>
2022-02-16 10:30:45 +0100

    intro

---
+++ js/test.js
@@ -0,0 +1,42 @@
+const readlineSync = require("readline-sync");
+
+//console.log("It works !!!");
+
+// ASK DATA TO THE USER
+// let userName = readlineSync.question('Can you give me your name please ?');
+// console.log("Hello " + userName);
+
+// VARIABLES
+// numbers
+// let age = 25;
+// age = 36;
+// age = 18;
+//phrase
+// let phrase = "Coding is fun!";
+// booleans
+// let isItTrue = true;
+// let isItFalse = false;
+
+// NUMBERS
+// let age = 23;
+// let sisterAge = age + 11;
+// age += 12; // age = age + 12 = 35
+// console.log(age);
+
+// STRINGS
+// let name = "Jean";
+// let nameSister = "Emma";
+// console.log("My name is " + name);
+// console.log("My name is " + nameSister);
+// let userName = readlineSync.question("What is your name ?");
+// console.log("He's called " + userName);
+
+// NEW NUMBER()
+// let htmlBadges = new Number(readlineSync.question('How many HTML badges do you have ?'));
+// let cssBadges = new Number(readlineSync.question('How many CSS badges do you have ?'));
+// let totalBadges = htmlBadges + cssBadges;
+// console.log('Woaw you have ' + totalBadges + " !");
+// new Number() converts a string to a number.
+// beware  of variable type !
+// console.log('10' + '5');
+// console.log(10 + 5);
