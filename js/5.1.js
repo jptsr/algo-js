1d54e2226b00fb1d8229f92bdb8026f1b50ed40a
Julie <juptsr.becode@gmail.com>
2022-02-17 14:13:48 +0100

    update

--- js/5.1.js
+++ js/5.1.js
@@ -7,19 +7,57 @@
 
 const readlineSync = require("readline-sync");
 
-let tvShow = {};
-function collectData(){
-    let q_name = readlineSync.question("What's your favorite TvShow ? ");
-    let q_year = new Number(readlineSync.question("What's the year of production ? "));
-    let q_members = readlineSync.question("What are the members' name ? ");
+// let tvShow = {};
+// function askTvSerie(){
+//     let q_name = readlineSync.question("What's your favorite TvShow ? ");
+//     let q_year = new Number(readlineSync.question("What's the year of production ? "));
+//     let q_members = readlineSync.question("What are the members' name ? ");
 
-    tvShow = {
-        name: q_name,
-        year: q_year,
-        members: q_members
+//     tvShow = {
+//         name: q_name,
+//         year: q_year,
+//         members: q_members
+//     }
+
+//     let jsonFile = JSON.stringify(tvShow, null, 2);
+//     return jsonFile;
+// }
+// console.log(askTvSerie());
+
+let q_name;
+let q_year;
+let howMany;
+let q_member;
+let q_members = [];
+let tvShow = {};
+function askTvSerie(){
+    q_name = readlineSync.question("What's your favorite TvShow ? ");
+    q_year = new Number(readlineSync.question("What's the year of production ? "));
+    howMany = new Number(readlineSync.question("How many members ? "));
+
+    if(howMany == 1){
+        q_member = readlineSync.question("What are the member's name ? ");
+        tvShow = {
+            name: q_name,
+            year: q_year,
+            member: q_member
+        }
+    }else if(howMany > 1){
+        for(let i = 0; i < howMany; i++){
+            q_members[i] = readlineSync.question("What are the members' name ? ");
+        }
+        
+        tvShow = {
+            name: q_name,
+            year: q_year,
+            members: q_members
+        }
+    }else{
+        let errorMsg = "Error";
+        return errorMsg;
     }
 
     let jsonFile = JSON.stringify(tvShow, null, 2);
     return jsonFile;
 }
-console.log(collectData());
+console.log(askTvSerie());
