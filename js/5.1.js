1421321fe56e3433756858f48ca7b6fadea6a785
Julie <juptsr.becode@gmail.com>
2022-02-17 13:50:46 +0100

    5.1

--- js/5.1.js
+++ js/5.1.js
@@ -5,6 +5,21 @@
 // Cette fonction doit rassembler toutes les données dans un seul objet et le renvoyer. La structure des données doit être élégante.
 // Créez un programme qui utilise cette fonction pour générer un objet de série télévisée et l'afficher à l'utilisateur au format JSON.
 
+const readlineSync = require("readline-sync");
+
+let tvShow = {};
 function collectData(){
-    
+    let q_name = readlineSync.question("What's your favorite TvShow ? ");
+    let q_year = new Number(readlineSync.question("What's the year of production ? "));
+    let q_members = readlineSync.question("What are the members' name ? ");
+
+    tvShow = {
+        name: q_name,
+        year: q_year,
+        members: q_members
+    }
+
+    let jsonFile = JSON.stringify(tvShow, null, 2);
+    return jsonFile;
 }
+console.log(collectData());
